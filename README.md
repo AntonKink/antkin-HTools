8c4f744f1f10c59afe189aef3a07fdede143246a
Anton Kink <kinkanton@gmail.com>
2020-11-23 14:10:30 +0200

    port scanner

---
+++ README.md
@@ -0,0 +1,10 @@
+# antkin-HTools
+
+These are just some of the tools I wrote for educational purposes. My goal is to learn how to create "useful" programs, to find out how they work, and also to learn how to prevent their harmful use.
+
+## Tools
+- Port Scanner
+
+## License
+These tools are provided using GPLv2 license. There is no liability and warranty.  
+These tools are provided for informational and educational purposes. The author is not responsible for their use.
---
+++ port_scanner/PS.py
@@ -0,0 +1,59 @@
+# -*- coding:utf -8 -*-
+
+from termcolor import colored
+import socket
+
+def fanc1():
+    color_a = colored("[+] ", 'green')
+    print("~"*50)
+    host = raw_input(color_a + "Host --> ")
+    port = int(raw_input(color_a + "Port --> "))
+    print("~"*50)
+
+    scan = socket.socket()
+
+    color_b = colored("[!] ", 'red')
+    color_c = colored("[!] ", 'yellow')
+
+    try:
+        scan.connect((host, port))
+    except socket.error:
+        print color_b + "Port -- ", port, " -- [CLOSED]"
+    else:
+        print color_c + "Port -- ", port, " -- [OPEN]"
+
+def fanc2():
+    color_a = colored("[+] ", 'green')
+    color_b = colored("[!] ", 'red')
+    color_c = colored("[!] ", 'yellow')
+
+    host = raw_input(color_a + "Host --> ")
+    print("\n")
+    port = [20, 21, 22, 23, 42, 43, 53, 67, 69, 80]
+
+    for i in port:
+        try:
+            scan = socket.socket()
+            scan.settimeout(0.5)
+            scan.connect((host, i))
+        except socket.error:
+            print color_b + "Port -- ", i, " -- [CLOSED]"
+        else:
+            print color_c + "Port -- ", i, " -- [OPEN]"
+
+print("~"*50)
+
+print("\tPort Scanner")
+print("\tOptions:")
+print("\t[1] --- scan a separate port")
+print("\t[2] --- scan list")
+
+print("~"*50 + "\n")
+text_a = input("[option]--> ")
+
+if text_a == 1:
+    fanc1()
+elif text_a == 2:
+    fanc2()
+else:
+    print(colored("Параметр введен не правильно!", 'red'))
